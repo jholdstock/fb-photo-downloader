7bb381a572f386ffe8f1553ff201ce2b56bdcd97
Jamie Holdstock <jam.jar69@gmail.com>
2017-05-11 17:23:50 +0100

    Remove progress bar duplication

--- conf.rb
+++ conf.rb
@@ -3,9 +3,14 @@ require 'highline/import'
 
 system "clear"
 
-@email = ""
+@beginning_time = Time.now
+
+@quick_mode = false
+@output_dir = "output"
+
 @email = ""
 puts "Using email: #{@email}", ""
 
 @pass = ""
 #@pass = ask("Enter password: ") { |q| q.echo = false }
+puts ""
--- download.rb
+++ download.rb
@@ -5,55 +5,66 @@ require 'selenium-webdriver'
 require 'pp'
 require 'securerandom'
 require 'fileutils'
+require 'open-uri'
 
 require './conf.rb'
 
-def download_file url, filename
-    url_base = url.split('/')[2]
-    url_path = '/'+url.split('/')[3..-1].join('/')
-    counter = 0
+class PB
+	def self.start total
+		puts ""
+		@@pbar = ProgressBar.create(:format => "%E |%w>%i| (%c / %C)",
+									:progress_mark  => "=",
+									:remainder_mark => " ",
+									:total => total)
+	end
 
-    Net::HTTP.start(url_base) do |http|
-        response = http.request_head(URI.escape(url_path))
-        pbar = ProgressBar.create(:format => "%a |%b>%i| %p%% %t",
-                                  :progress_mark  => "=",
-                                  :remainder_mark => " ")
-        fileSize = response['content-length'].to_f
+	def self.increment
+		@@pbar.increment
+	end
 
-        File.open(filename, 'w') do |f|
-            http.get(URI.escape(url_path)) do |str|
-                f.write str
-                counter += str.length.to_f 
-                pbar.progress = (counter/fileSize)*100
-            end
-        end
-        pbar.finish
-    end
+	def self.stop
+		@@pbar.finish
+		puts "", ""
+	end
+end
+
+def download_files hq_links
+	puts "", "Download HQ photos... "
+	PB.start hq_links.length
+	hq_links.each do |filename, url|
+		filename = "#{@output_dir}/#{filename}.jpg"
+
+		open(filename, 'wb') do |file|
+			file << open(url).read
+		end
+
+		PB.increment
+	end
+	PB.stop
 end
 
 def create_driver
 	caps = Selenium::WebDriver::Remote::Capabilities.chrome(
-	  "chromeOptions" => {
-	    "args" => [
-	      "--disable-web-security",
-	      "--incognito"
-	    ],
-	  }
-	)
+	"chromeOptions" => {
+		"args" => [
+			"--disable-web-security",
+			"--incognito"
+		],
+	})
 	return Selenium::WebDriver.for :chrome, desired_capabilities: caps
 end
 
 def login
-	print "Opening Chrome... "  
+	print "", "Opening Chrome... "  
 	@driver.get "https://www.facebook.com"
-	puts "done\n", ""
+	puts "done\n", "", ""
 
 
 	print "Entering user ID and password... "  
 	@driver.find_element(:id => "email").send_keys @email
 	@driver.find_element(:id => "pass").send_keys @pass
 	@driver.find_element(:id => "pass").send_keys :enter
-	puts "done\n", ""
+	puts "done\n", "", ""
 end
 
 def get_username
@@ -64,28 +75,24 @@ def get_username
 end
 
 def open_all_years
-	puts "Open all years...", ""
+	puts "", "Open all years..."
 	@driver.get "https://m.facebook.com/#{@username}/allactivity?log_filter=cluster_200"
 	async_elements = @driver.find_elements(:css => ".timeline > div > div .async_elem")
-	inc = 0
-	pbar = ProgressBar.create(:format => "%a |%b>%i| %p%% %t",
-                      :progress_mark  => "=",
-                      :remainder_mark => " ")
-	async_elements.each do |element|
-	  text = element.text
-	  next if text !~ /^\d{4}$/
-	  element.click
-	  #debug puts "\t#{text}"
-	  sleep 0.5
-	  inc += 1
- 	  pbar.progress = (inc.to_f/async_elements.length.to_f)*100
-	  break if @quick_mode and inc >=2
+	PB.start async_elements.length
+	async_elements.each_with_index do |element, i|
+		text = element.text
+		next if text !~ /^\d{4}$/
+		element.click
+		#debug puts "\t#{text}"
+		sleep 0.5
+		PB.increment
+		break if @quick_mode and i >=2
 	end
-	pbar.finish
+	PB.stop
 end
 
 def open_all_months
-	puts "", "", "Open all months...", ""
+	puts "", "Open all months..."
 	async_elements = @driver.find_elements(:css => ".timeline > div > div .collapse")
 
 # I think this is a breaking change. Only found 332 photos running like this. Missing or dupes?
@@ -93,50 +100,69 @@ def open_all_months
 	async_elements.delete_if { |x| x.text =~ /^No stories available$/ }
 
 
-
-
-	inc = 0
-	pbar = ProgressBar.create(:format => "%a |%b>%i| %p%% %t",
-                      :progress_mark  => "=",
-                      :remainder_mark => " ")
-	async_elements.each do |element|
-	  text = element.text
-	  element.click
-	  #debug puts "\t#{text}"
-	  sleep 0.5
-	  inc += 1
- 	  pbar.progress = (inc.to_f/async_elements.length.to_f)*100
-	  break if @quick_mode and inc >=2
+	PB.start async_elements.length
+	async_elements.each_with_index do |element, i|
+		text = element.text
+		element.click
+		#debug puts "\t#{text}"
+		sleep 0.5
+		PB.increment 
+		break if @quick_mode and i >=2
 	end
-	pbar.finish
+	PB.stop
 end
 
 
 def expand_months
-	puts "", "", "Fully expand months...", ""
+	puts "", "Fully expand months..."
 	loop do
-	  async_elements = @driver.find_elements(:css => ".timeline > div > div > div > div .async_elem")
-	  async_elements.delete_if { |x| x.text =~ /^No more from/ }
-	  break if async_elements.length == 0
-
-    	inc = 0
-    	pbar = ProgressBar.create(:format => "%a |%b>%i| %p%% %t",
-                      :progress_mark  => "=",
-                      :remainder_mark => " ")
-	    async_elements.each do |element|
-	    text = element.text
-	    element.click
-	    # debug puts "\t#{text}"
-	    sleep 0.5
-	    inc += 1
- 	  	pbar.progress = (inc.to_f/async_elements.length.to_f)*100
-	  end
-	  pbar.finish
+		async_elements = @driver.find_elements(:css => ".timeline > div > div > div > div .async_elem")
+		async_elements.delete_if { |x| x.text =~ /^No more from/ }
+		break if async_elements.length == 0
+		
+		PB.start async_elements.length
+		async_elements.each_with_index do |element, i|
+			text = element.text
+			element.click
+			# debug puts "\t#{text}"
+			sleep 0.5
+			PB.increment
+		end
+		PB.stop
 	end
 end
 
-@quick_mode = false
-@output_dir = "output"
+def scan_for_tags
+	puts "", "Scan page for tags... "
+	photo_links = @driver.find_elements(:xpath => "//div[@class='timeline']//a[text()='photo']")
+	photo_links.collect! { |x| x.attribute("href") }
+	puts "\t Tagged in #{photo_links.length} photos", "", ""    
+	return photo_links
+end
+
+def get_hq_links photo_links
+	puts "", "Get HQ photo links... "
+	hq_links = {}
+	PB.start photo_links.length
+	photo_links.each_with_index do |link, i|
+		@driver.get link
+		img_url = @driver.find_element(:xpath, './/a[contains(., "View full size")]').attribute("href")
+		json = @driver.find_element(:css, 'div > span > div > div > abbr').attribute("data-store")
+		name = @driver.find_element(:css, 'div > div > a > strong.actor').text
+		json_hash = JSON.parse json
+		time = json_hash["time"]
+		time = Time.at(time)
+		filename = time.to_date.to_s + " " + name + " - " + @driver.title + " " + SecureRandom.hex(1)
+		filename.gsub!("-", ':')
+		filename.gsub!(/[^:0-9A-Za-z\s]/, '')
+		filename.gsub!(":", '-')
+		hq_links[filename] = img_url
+		PB.increment 
+		break if @quick_mode and i >=10
+	end
+	PB.stop 
+	return hq_links
+end
 
 @driver = create_driver
 login
@@ -145,65 +171,17 @@ open_all_years
 open_all_months
 expand_months
 
-
-puts "", "Scan page for tags... "
-photo_links = @driver.find_elements(:xpath => "//div[@class='timeline']//a[text()='photo']")
-photo_links.collect! { |x| x.attribute("href") }
-puts "\t Tagged in #{photo_links.length} photos", ""
-
-# post_links = links.select { |x| x.text == "post" }
-# post_links.collect!  { |x| x.attribute("href") }
-# puts "\t Tagged in #{post_links.length} posts", ""
-
-puts "Get HQ photo links... "
-hq_links = {}
-
-print "\tTagged photos... "
-inc = 0
-pbar = ProgressBar.create(:format => "%a |%b>%i| %p%% %t",
-                      :progress_mark  => "=",
-                      :remainder_mark => " ")
-photo_links.each do |link|
-  @driver.get link
-  img_url = @driver.find_element(:xpath, './/a[contains(., "View full size")]').attribute("href")
-  json = @driver.find_element(:css, 'div > span > div > div > abbr').attribute("data-store")
-  name = @driver.find_element(:css, 'div > div > a > strong.actor').text
-  json_hash = JSON.parse json
-  time = json_hash["time"]
-  time = Time.at(time)
-  fileName = time.to_date.to_s + " " + name + " - " + @driver.title + " " + SecureRandom.hex(1)
-  fileName.gsub!("-", ':')
-  fileName.gsub!(/[^:0-9A-Za-z\s]/, '')
-  fileName.gsub!(":", '-')
-  hq_links[fileName] = img_url
-  inc += 1
-  pbar.progress = (inc.to_f/photo_links.length.to_f)*100
-  break if @quick_mode and inc >=10
-end
-pbar.finish
-
-# print "\tTagged posts... "
-# post_links.each do |link|
-# 	@driver.get link
-# 	full_size = @driver.find_elements(:css, 'div > div >  a > div > div i')
-# 	pp full_size
-# 	sleep 10000
-
-# 	if full_size.length == 0
-# 		puts "No photo - skipping"
-# 		next
-# 	end
-# 	puts "TODO Get date and photo url. Add to hq_links hash"
-# 	sleep 30000
-# end
-# puts "done"
+photo_links = scan_for_tags
+hq_photo_links = get_hq_links photo_links
 
 FileUtils.rm_rf(@output_dir)
 Dir.mkdir(@output_dir)
 
-puts "", "Download HQ photos... "
-hq_links.each do |key, value|
-    download_file value, "output/"+key+".jpg"
-end
+download_files hq_photo_links
 
-puts "", "--------", "FINISHED", "--------"
+
+@end_time = Time.now
+elapsed = @end_time - @beginning_time
+sec = elapsed % 60
+min = elapsed / 60
+puts "---------------------------", " Finished in #{min.floor} min #{sec.floor} sec", "---------------------------", "", ""
